4ad2cf43b24dbe6afbbe764eecb3e60172585df1
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-10-14 20:17:18 +0200

    start on shadowing

--- SmaleSpace/HyperbolicMap.lean
+++ SmaleSpace/HyperbolicMap.lean
@@ -52,3 +52,65 @@ instance [h : HasRuelleBracketWithMap X] : HasRuelleBracketWithMap (invDyn X) wh
     · apply Equiv.apply_symm_apply
   expansion x y hxy := h.contraction (ofInvDyn x) (ofInvDyn y) hxy
   contraction x y hxy := h.expansion (ofInvDyn x) (ofInvDyn y) hxy
+
+lemma dist_image_iter_le_of_pseudoOrbit
+    {Y : Type*} [MetricSpace Y] {f : Y → Y} (hf : UniformContinuous f)
+    {δ : ℝ} (hδ : 0 < δ) (n : ℕ) :
+    ∃ ε > 0, ∀ (u : ℕ → Y), (∀ n, dist (f (u n)) (u (n + 1)) < ε) →
+      dist (f ^[n] (u 0)) (u n) < δ := by
+  induction n generalizing δ with
+  | zero =>
+    refine ⟨δ, hδ, fun u hu ↦ by simp [hδ]⟩
+  | succ n ih =>
+    rcases Metric.uniformContinuous_iff.1 hf (δ / 2) (by linarith) with ⟨δ', δ'pos, h'⟩
+    rcases ih δ'pos with ⟨ε, εpos, hε⟩
+    refine ⟨min ε (δ / 2), by positivity, fun u hu ↦ ?_⟩
+    calc
+    dist (f^[n + 1] (u 0)) (u (n + 1))
+    _ ≤ dist (f (f^[n] (u 0))) (f (u n)) + dist (f (u n)) (u (n + 1)) := by
+      rw [iterate_succ_apply']
+      apply dist_triangle
+    _ < δ / 2 + δ / 2 := by
+      gcongr
+      · apply h'
+        exact hε u (fun m ↦ (hu m).trans_le (min_le_left _ _))
+      · exact (hu n).trans_le (min_le_right _ _)
+    _ = δ := by linarith
+
+variable [HasRuelleBracketWithMap X] [HasReduceScale X]
+
+lemma future_shadowing_aux (ε δ : ℝ) (hε : 0 < ε) (hδ : 0 < δ) (x : ℕ → X)
+    (hx : ∀ n, dist (T (x n)) (x (n+1)) < ε) (M : ℕ) (hM : 2 * λ ^ M * δ ≤ reduceScale X δ)
+    (hε : ∀ (u : ℕ → X), (∀ n, dist (T (u n)) (u (n + 1)) < ε) →
+      dist (T ^[M] (u 0)) (u M) < reduceScale X δ / 2) :
+    ∃ p ∈ locUnstable ε (x 0), ∀ n, dist (x n) (T ^[n] p) < ε := by
+  -- define inductively a sequence by `y₀ = x₀`, and `y_{n+1}` the intersection of `W^u (T^M yₙ)`
+  -- and `W^s (x_{M * (n+1)})`.
+  let y := Nat.rec (motive := fun n ↦ X) (x 0) (fun n q ↦ ⁅T^[M] q, x (M * (n + 1))⁆)
+  have A n : dist (y n) (x (M * n)) < δ := by
+    induction n with
+    | zero =>
+      simp [y, hδ]
+    | succ n ih =>
+      have : y (n + 1) = ⁅T^[M] (y n), x (M * (n + 1))⁆ := rfl
+      rw [this, dist_comm]
+      apply dist_bracket_lt_of_lt_reduceScale ?_ (by simp [reduceScale_pos hδ])
+      apply (dist_triangle_left _ _ (T^[M] (x (M * n)))).trans_lt
+      have A : dist (T^[M] (x (M * n))) (x (M * (n + 1))) < reduceScale X δ / 2 :=
+        hε (fun k ↦ x (M * n + k)) (fun k ↦ hx (M * n + k))
+      have B : dist (T^[M] (x (M * n))) (T^[M] (y n)) ≤ λ ^ M * δ := sorry
+      linarith
+
+
+
+
+
+
+
+
+
+lemma future_shadowing (ε : ℝ) (hε : 0 < ε) (x : ℕ → X) (hx : ∀ n, dist (x (n+1)) (T (x n)) ≤ ε) :
+    ∃ p ∈ locUnstable ε (x 0), ∀ n, dist (x n) (T ^[n] p) < ε := by
+  -- define inductively a sequence by `y₀ = x₀`, and `y_{n+1}` is the intersection of `W^u (T yₙ)`
+  -- and `W ^ s (x_{n+1})`.
+  let y := Nat.rec (motive := fun n ↦ X) (x 0) (fun n q ↦ ⁅T q, x (n + 1)⁆)
