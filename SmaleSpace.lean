d9035a92e7efe15195fe8e8ac7904922625e557a
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-11-01 20:43:56 +0100

    rename file

--- SmaleSpace.lean
+++ SmaleSpace.lean
@@ -1,6 +1,6 @@
-import SmaleSpace.FST
 import SmaleSpace.LocMaximal
 import SmaleSpace.Obsolete.Bracket
 import SmaleSpace.Obsolete.BracketUnif
 import SmaleSpace.Obsolete.HyperbolicMap
 import SmaleSpace.PiInt
+import SmaleSpace.SFT
